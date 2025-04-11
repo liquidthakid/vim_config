acc5ee679f632ba9c63aaa884fd82322090ce8bd
Timothy Cahill <timothy.cahill@wbd.com>
2025-04-11 16:15:34 -0400

    Set tab length to 4 for text files

--- init.lua
+++ init.lua
@@ -106,3 +106,12 @@ cmp.setup({
     end,
   },
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "text",
+	callback = function()
+		vim.opt_local.tabstop = 4
+		vim.opt_local.shiftwidth = 4
+		vim.opt_local.expandtab = true
+	end,
+})
